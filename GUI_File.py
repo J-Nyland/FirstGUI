99d7cfdcbe4465bc687a584295c6e00f7877c80a
Jonathan Nyland <jonathan.nyland@cmsdistribution.com>
2020-03-02 17:28:30 +0000

    Using GUI

---
+++ GUI_File.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+ThisIsTheLabel = Label(root, text = "This is a popup window")
+ThisIsTheLabel.pack()
+
+root.mainloop()
+
+#lines 3 and 8 are necessary for the baic layout
